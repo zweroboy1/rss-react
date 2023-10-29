2b879279c7f6eca0ec83efdd1be2ad8be8ba3f0e
zweroboy1 <fprognoz@gmail.com>
2023-10-29 20:04:12 +0200

    fix: readme.md

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # rss-react
 
-Учебный проект на React.
+## Учебный проект на React.
+
 Задания тут: https://github.com/rolling-scopes-school/tasks/tree/master/react/modules
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import viteLogo from '/logo.svg';
-import './App.css';
+    import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+
+
+
+  const [count, setCount] = useState(0)
 
   return (
-    <div>Movies</div>
-  );
+    <div>Movies</div>);
 }
 
 export default App;
