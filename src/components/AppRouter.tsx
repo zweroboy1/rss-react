8e3238b179c6aef53ccf6706a65d0cdb493d972c
zweroboy1 <fprognoz@gmail.com>
2023-11-07 01:55:29 +0200

    feat: add 404 route

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Main from '../pages/Main';
 import { BeerBottle } from '../components/BeerBottle';
+import { NotFound } from '../components/NotFound';
 
 const AppRouter = () => {
   return (
@@ -8,6 +9,7 @@ const AppRouter = () => {
       <Route path="/" element={<Main />}>
         <Route path="/item/:productId" element={<BeerBottle />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
